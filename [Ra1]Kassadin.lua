a2b0c7247a6f809b5b3f6d98fc5c86adaad55778
Patryk Krawczyk <patrykekrawczyk@gmail.com>
2018-07-06 16:47:51 +0000

    Add new file

---
+++ [Ra1]Kassadin.lua
@@ -0,0 +1,140 @@
+
+
+-- Menu
+local ra1Kassadin = Menu('Kassadin','[Ra1] Kassadin')
+
+-- Combo
+ra1Kassadin:SubMenu("Combo", "Combo Settings")
+ra1Kassadin.Combo:Boolean("Q", "Use Q", true)
+ra1Kassadin.Combo:Boolean("W", "Use W", true)
+ra1Kassadin.Combo:Boolean("E", "Use E", true)
+ra1Kassadin.Combo:Boolean("R", "Use R", true)
+ra1Kassadin.Combo:DropDown("ComboSeq","Combo Type", 2, {"Q => R => W => E", "W => Q => R=> E"})
+
+-- Harras
+ra1Kassadin:SubMenu("Harass", "Harass Settings")
+ra1Kassadin.Harass:Boolean("Q", "Use Q", true)
+ra1Kassadin.Harass:Boolean("W", "Use W", true)
+ra1Kassadin.Harass:Boolean("E", "Use E", true)
+
+-- Drawings
+ra1Kassadin:SubMenu("Draw", "Draw")
+ra1Kassadin.Draw:Boolean("Q", "Draw Q", true)
+ra1Kassadin.Draw:Boolean("W", "Draw W", true)
+ra1Kassadin.Draw:Boolean("E", "Draw E", true)
+ra1Kassadin.Draw:Boolean("R", "Draw R", true)
+ra1Kassadin.Draw:Boolean("Disable", "Disable all", false)
+
+local SpellsRange = {
+    Q = 650,
+    W = 300,
+    E = 700,
+    R = 500,
+}
+
+-- Orbwalker
+function Mode()
+	if _G.IOW_Loaded and IOW:Mode() then
+		return IOW:Mode()
+	elseif _G.PW_Loaded and PW:Mode() then
+		return PW:Mode()
+	elseif _G.DAC_Loaded and DAC:Mode() then
+		return DAC:Mode()
+	elseif _G.AutoCarry_Loaded and DACR:Mode() then
+		return DACR:Mode()
+	elseif _G.SLW_Loaded and SLW:Mode() then
+		return SLW:Mode()
+	elseif GoSWalkLoaded and GoSWalk.CurrentMode then
+		return ({"Combo", "Harass", "LaneClear", "LastHit"})[GoSWalk.CurrentMode+1]
+	end
+end
+
+-- Drawings
+function Draw(myHero)
+    if myHero.dead or ra1Kassadin.Draw.Disable:Value() then return end
+    local pos = GetOrigin(myHero)
+    if ra1Kassadin.Draw.Q:Value() then
+        DrawCircle(pos, SpellsRange.Q, 1, 100, 0xFFF97F51)
+    end
+    if ra1Kassadin.Draw.W:Value() then
+        DrawCircle(pos, SpellsRange.E, 1, 100, 0xFF82589F)
+    end
+    if ra1Kassadin.Draw.E:Value() then
+        DrawCircle(pos, SpellsRange.W, 1, 100, 0xFF1B9CFC)
+    end
+    if ra1Kassadin.Draw.R:Value() then
+        DrawCircle(pos, SpellsRange.R, 2, 100, 0xFF182C61)
+    end
+end
+
+function getDistance(trg)
+    local enemyTarget = trg
+    return math.sqrt( (myHero.x - enemyTarget.x)^2 + (myHero.z - enemyTarget.z)^2 )
+end
+
+function castQ()
+    CastTargetSpell(target, _Q)
+end
+
+function castW()
+    if getDistance(target) < 400 then
+        CastSpell(_W)
+        AttackUnit(target)
+    end
+end
+
+function castE()
+    if getDistance(target) < SpellsRange.E then
+        local targetPos = GetOrigin(target)
+        CastSkillShot(_E, targetPos)
+    end
+end
+
+function castR()
+    if getDistance(target) < SpellsRange.R then
+        CastTargetSpell(target, _R)
+    end
+end
+
+function Combo()
+    if Mode() == 'Combo' then
+        if ra1Kassadin.Combo.Q:Value() and Ready(_Q) then
+            castQ()
+        end
+        if ra1Kassadin.Combo.R:Value() and Ready(_R) then
+            castR()
+        end
+        if ra1Kassadin.Combo.W:Value() and Ready(_W) then
+            castW()
+        end
+        if ra1Kassadin.Combo.E:Value() and Ready(_E) then
+            castE()
+        end
+    end
+end
+
+function Harass()
+    if Mode() == 'Harass' then
+        if ra1Kassadin.Harass.Q:Value() and Ready(_Q) then
+            castQ()
+        end
+        if ra1Kassadin.Harass.W:Value() and Ready(_W) then
+            castW()
+        end
+        if ra1Kassadin.Harass.E:Value() and Ready(_E) then
+            castE()
+        end
+    end
+end
+
+OnTick(function()
+    target = GetCurrentTarget()
+    Combo()
+    Harass()
+    end
+)
+
+OnDraw(function(myHero)
+        Draw(myHero)
+    end
+)
